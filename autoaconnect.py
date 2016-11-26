fcce083677623bec029a498bd67caed0ca4b87c6
Septolum <nat.marriage@gmail.com>
2016-11-26 15:39:19 +0000

    Added infinite monitoring loop

--- autoaconnect.py
+++ autoaconnect.py
@@ -1,4 +1,19 @@
 #! /usr/bin/python3
 import subprocess, re
-for i in (re.findall("client (\d+): '(?!(?:Midi Through)|(?:FLUID Synth))", subprocess.run(["aconnect", "-o"], universal_newlines=True, stdout=subprocess.PIPE).stdout, re.I)):
-    subprocess.run(["aconnect", i + ":0", (re.findall("client (\d+): '(?=FLUID Synth)", subprocess.run(["aconnect", "-o"], universal_newlines=True, stdout=subprocess.PIPE).stdout, re.I))[0] + ":0"])
+from time import sleep
+
+def connectall():
+    for i in (re.findall("client (\d+): '(?!(?:Midi Through)|(?:FLUID Synth))", subprocess.run(["aconnect", "-o"], universal_newlines=True, stdout=subprocess.PIPE).stdout, re.I)):
+        subprocess.run(["aconnect", i + ":0", (re.findall("client (\d+): '(?=FLUID Synth)", subprocess.run(["aconnect", "-o"], universal_newlines=True, stdout=subprocess.PIPE).stdout, re.I))[0] + ":0"])
+        #print("Connected " + str(i))
+
+connectall()
+
+prev = ""
+ 
+while True:
+    sleep(5)
+    new = subprocess.run(["aconnect", "-o"], universal_newlines=True, stdout=subprocess.PIPE).stdout
+    if prev != new:
+        connectall()
+    prev = new
